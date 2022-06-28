4dc2ae4c7b77e8b4987e09661b0817ca5f1b0eb4
SanmaTairyo <33730921+Tsumoridesu@users.noreply.github.com>
2022-06-28 14:47:01 +0900

    Add files via upload

--- src/predict_for_camera.py
+++ src/predict_for_camera.py
@@ -15,6 +15,7 @@ import torch.nn as nn
 from torchvision import transforms as T
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from deeplabv3_plus_pytorch_ros.msg import zone_status
 import network
 import utils
 from datasets import VOCSegmentation, Cityscapes, cityscapes
@@ -60,6 +61,7 @@ class image_segmentation:
         # Ros options
         self.image_pub_topic = rospy.get_param('~image_publish_topic', 'segmentation_image')
         self.image_pub = rospy.Publisher(self.image_pub_topic, Image, queue_size=1)
+        self.zone_status_pub = rospy.Publisher('%s/zone_status' % self.image_pub_topic, zone_status, queue_size=1)
 
         self.bridge = CvBridge()
 
@@ -81,6 +83,36 @@ class image_segmentation:
         # Set up model (all models are 'constructed at network.modeling)
         self.model = network.modeling.__dict__[self.model_mode](num_classes=self.num_classes,
                                                                 output_stride=self.output_stride)
+
+        self.zone_status_out = rospy.get_param('~zone_status_out', False)
+
+        if self.zone_status_out:
+            # 左側
+            left_mask = np.zeros([480, 640], np.uint8)
+            points_left = np.array([[220, 240], [320, 240], [320, 480], [48, 480]])
+            self.left_zone = cv2.fillPoly(left_mask, [points_left], color=255)
+            self.sum_left_zone = np.sum(self.left_zone)
+
+            # 右側
+            right_mask = np.zeros([480, 640], np.uint8)
+            points_right = np.array([[320, 240], [420, 240], [592, 480], [320, 480]])
+            self.right_zone = cv2.fillPoly(right_mask, [points_right], color=255)
+            self.sum_right_zone = np.sum(self.right_zone)
+
+            # 直進危険領域
+            danger_mask = np.zeros([480, 640], np.uint8)
+            points_danger = np.array([[227, 360], [413, 360], [456, 480], [184, 480]])
+            self.danger_zone = cv2.fillPoly(danger_mask, [points_danger], color=255)
+            self.sum_danger_zone = np.sum(self.danger_zone)
+
+            # 直進注意領域
+            warning_mask = np.zeros([480, 640], np.uint8)
+            points_warning = np.array([[270, 240], [370, 240], [413, 360], [227, 360]])
+            self.warning_zone = cv2.fillPoly(warning_mask, [points_warning], color=255)
+            self.sum_warning_zone = np.sum(self.warning_zone)
+
+            rospy.loginfo("zone_status_pub True")
+
         if self.separable_conv and 'plus' in self.model_mode:
             network.convert_to_separable_conv(self.model.classifier)
         utils.set_bn_momentum(self.model.backbone, momentum=0.01)
@@ -91,10 +123,10 @@ class image_segmentation:
             self.model.load_state_dict(checkpoint["model_state"])
             model = nn.DataParallel(self.model)
             model.to(self.device)
-            print("Resume model from %s" % self.ckpt)
+            rospy.loginfo("Resume model from %s" % self.ckpt)
             del checkpoint
         else:
-            print("[!] Retrain")
+            rospy.loginfo("[!] Retrain")
             self.model = nn.DataParallel(self.model)
             self.model.to(self.device)
         with torch.no_grad():
@@ -126,6 +158,36 @@ class image_segmentation:
         pred = pred.max(1)[1].cpu().numpy()[0]
         colorized_preds = self.decode_fn(pred).astype('uint8')
         colorized_preds = cv2.cvtColor(np.asarray(colorized_preds), cv2.COLOR_RGB2BGR)
+
+        if self.zone_status_out:
+            # 通行可能領域抽出
+            road_rgb = np.array([128, 64, 128])
+            mask = cv2.inRange(colorized_preds, lowerb=road_rgb, upperb=road_rgb)
+
+            # ノイズ消去
+            kel_dil = np.ones((17, 17), np.uint8)
+            kel_ero = np.ones((17, 17), np.uint8)
+            # 膨張
+            dil = cv2.dilate(mask, kel_dil)
+            # 腐食
+            ero = cv2.erode(dil, kel_ero)
+
+            right_zone_img = cv2.bitwise_and(ero, ero, mask=self.right_zone)
+            left_zone_img = cv2.bitwise_and(ero, ero, mask=self.left_zone)
+            danger_zone_img = cv2.bitwise_and(ero, ero, mask=self.danger_zone)
+            warning_zone_img = cv2.bitwise_and(ero, ero, mask=self.warning_zone)
+
+            danger_zone_point = np.sum(danger_zone_img) / self.sum_danger_zone
+            right_zone_point = np.sum(right_zone_img) / self.sum_right_zone
+            left_zone_point = np.sum(left_zone_img) / self.sum_left_zone
+            warning_zone_point = np.sum(warning_zone_img) / self.sum_warning_zone
+
+            msg = zone_status()
+            msg.danger_zone_point = danger_zone_point
+            msg.right_zone_point = right_zone_point
+            msg.left_zone_point = left_zone_point
+            msg.warning_zone_point = warning_zone_point
+            self.zone_status_pub.publish(msg)
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(colorized_preds, "bgr8"))
         except CvBridgeError as e:
